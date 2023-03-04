7c07d3aac0a6bd6e19cb691690bf2c125838e375
Jazmin Gonzalez <gonzalez.jazmin@student.greenriver.edu>
2023-03-03 19:35:32 -0500

    finished adding getters and setters to applicant class

--- classes/applicant.php
+++ classes/applicant.php
@@ -5,6 +5,7 @@ class Applicants
 {
     private $_fname;
     private $_lname;
+    private $_email;
     private $_state;
     private $_phone;
     private $_github;
@@ -61,5 +62,138 @@ class Applicants
     public function setLname($lname)
     {
         $this->_lname = $lname;
+    }
+
+    /**
+     * getEmail returns the email name of the applicant
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+
+    /**
+     * getEmail sets the email of the applicant
+     * @param $email string
+     *
+     */
+    public function setEmail($email)
+    {
+        $this->_email = $email;
+    }
+
+    /**
+     * getEmail returns the email name of the applicant
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->_state;
+    }
+
+    /**
+     * getEmail sets the email of the applicant
+     * @param $state string
+     *
+     */
+    public function setState($state)
+    {
+        $this->_state = $state;
+    }
+
+    /**
+     * getPhone returns the email name of the applicant
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->_phone;
+    }
+
+    /**
+     * getPhone sets the email of the applicant
+     * @param $phone string
+     *
+     */
+    public function setPhone($phone)
+    {
+        $this->_phone = $phone;
+    }
+
+    /**
+     * getGitHub returns the email name of the applicant
+     * @return string
+     */
+    public function getGitHub()
+    {
+        return $this->_github;
+    }
+
+    /**
+     * getGitHub sets the email of the applicant
+     * @param $github string
+     *
+     */
+    public function setGitHub($github)
+    {
+        $this->_github = $github;
+    }
+
+    /**
+     * getExperience returns the email name of the applicant
+     * @return string
+     */
+    public function getExperience()
+    {
+        return $this->_experience;
+    }
+
+    /**
+     * getExperience sets the email of the applicant
+     * @param $expereince string
+     *
+     */
+    public function setExperience(string $expereince)
+    {
+        $this->_experience = $expereince;
+    }
+
+    /**
+     * getRelocate returns the email name of the applicant
+     * @return string
+     */
+    public function getRelocate()
+    {
+        return $this->_relocate;
+    }
+
+    /**
+     * getRelocate sets the email of the applicant
+     * @param $relocate string
+     *
+     */
+    public function setRelocate(string $relocate)
+    {
+        $this->_relocate = $relocate;
+    }
+
+    /**
+     * getBio returns the email name of the applicant
+     * @return string
+     */
+    public function getBio()
+    {
+        return $this->_bio;
+    }
+
+    /**
+     * getBio sets the email of the applicant
+     * @param $bio string
+     *
+     */
+    public function setBio(string $bio)
+    {
+        $this->_bio = $bio;
     }
 }
